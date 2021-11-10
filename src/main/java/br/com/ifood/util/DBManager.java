bcddba3df29f5e5d34e6414222e511019bccafc1
Riane <riane.dossantos75@gmail.com>
2021-11-09 23:35:14 -0300

    servlets added

--- src/main/java/br/com/ifood/util/DBManager.java
+++ src/main/java/br/com/ifood/util/DBManager.java
@@ -8,9 +8,9 @@ public class DBManager {
 
 	public static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
 
-	public static final String USER = "";
+	public static final String USER = "RM87890";
 
-	public static final String PASSWORD = "";
+	public static final String PASSWORD = "Dtnasc#030503";
 
 	public static Connection getConnection() {
 
